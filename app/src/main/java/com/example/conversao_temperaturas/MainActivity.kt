d5ea3d3e74c3d8655c2fffcfbba1d48eff12b412
Henrique Fernandes <henrique.fquick@gmail.com>
2019-08-19 23:45:56 -0300

    Correcao e Comentarios
    Correcao de erros de logica no codigo MainActivity..kt e adicionados alguns comentarios ao codigo

--- app/src/main/java/com/example/conversao_temperaturas/MainActivity.kt
+++ app/src/main/java/com/example/conversao_temperaturas/MainActivity.kt
@@ -11,51 +11,58 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var result : String = ""
+    private var tmp : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        var temp : Double = edt_Temperatura.text.toString().toDouble()
-
-
-        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+        //verifica constantemente se o radiogroup foi alterado
+        radioGroup.setOnCheckedChangeListener { _ , checkedId ->
 
             val radio: RadioButton = findViewById(checkedId)
 
-
+            //calculo da temperatura para cada opcao apresentada
             when (radio.id) {
-                R.id.op_Kelvin -> {
-                    //Toast.makeText(this, "$temp", Toast.LENGTH_LONG).show()
-                    result = (temp + 273.15).toString()
-                    //Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+                R.id.op_Kelvin -> tmp = 1
+                R.id.op_Fahrenheit -> tmp = 2
+                R.id.op_Reaumur -> tmp = 3
+                R.id.op_Rankine -> tmp = 4
+                else -> {
+                    Toast.makeText(this, "Opcao Invalida", Toast.LENGTH_SHORT).show()
+                    tmp = 0
                 }
-                R.id.op_Fahrenheit -> result = ((temp * 9 / 5) + 32).toString()
-                R.id.op_Reaumur -> result = (temp * 4 / 5).toString()
-                R.id.op_Rankine -> result = ((temp * 9 / 5) + 491.67).toString()
-                else -> result = "a"
             }
 
         }
 
+        //listener do botao ( redireciona para o metodo onClick)
         btn_Converter.setOnClickListener(this)
 
     }
 
+    //metodo ativado quando o botao eh pressionado
+    //metodo coloca o resultado da conta pela temperatura selecionada no txt_Resultado
     override fun onClick(v: View?) {
 
+        // guarda o valor digitado no edt_Temperatura
+        var temp : Double = edt_Temperatura.text.toString().toDouble()
+
+        var result : String = ""
+        when (tmp) {
+            1 -> result = (temp + 273.15).toString()
+            2 -> result = ((temp * 9 / 5) + 32).toString()
+            3 -> result = (temp * 4 / 5).toString()
+            4 -> result = ((temp * 9 / 5) + 491.67).toString()
+            else -> {
+                Toast.makeText(this, "Opcao Invalida", Toast.LENGTH_SHORT).show()
+                result = "0"
+            }
+        }
 
         when(v?.id){
 
-            R.id.btn_Converter -> {
-
-                //Toast.makeText(this, result, Toast.LENGTH_LONG).show()
-
-                txt_Resultado.text = result
-
-            }
+            R.id.btn_Converter -> txt_Resultado.text = result
         }
     }
 }
